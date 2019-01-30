f3b93dbf1a6b18de640850f12b89a3e7fcb7eb68
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-01-30 01:01:19 -0300

    navbar nom style

---
+++ src/components/Header/index.js
@@ -0,0 +1,8 @@
+// importação do react
+import React from 'react';
+// a sintaxe abaixo refere-se a ES6
+const Header = () => (
+    <h1>Hi again</h1>
+);
+// exportando
+export default Header;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,25 @@
+// React 
+import React, {Component} from 'react';
+// style
+import "./style.css";
+import logo from "./logo.png";
+class Navbar extends Component {
+    render() {
+        return (
+            <nav>
+                <a classname="Navbar-brand">
+                    <img id="nav-brand" src= {logo} alt="logo kinvo"></img>
+                </a>
+                <ul>
+                    <li id="navlink"><a href="#sobre">Sobre o kinvo</a></li>
+                    <li id="navlink"><a href="#recursos">Recuros avançados</a></li>
+                    <li id="navlink"><a href="#kinvoweb">Kinvo Web</a></li>
+                    <li id="navlink"><a href="modal_sigin">Cadastre-se</a></li>
+                    <li id="navlink"><a href="modal_login">Entrar</a></li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
