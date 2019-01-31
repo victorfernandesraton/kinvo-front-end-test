43df032031fa256dca27d9175d37f60de224ac3a
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-01-31 08:34:01 -0300

    navbar nom-mutation

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -14,8 +14,8 @@ class Navbar extends Component {
                     <li id="navlink"><a href="#sobre">Sobre o kinvo</a></li>
                     <li id="navlink"><a href="#recursos">Recuros avançados</a></li>
                     <li id="navlink"><a href="#kinvoweb">Kinvo Web</a></li>
-                    <li id="navlink"><a href="modal_sigin">Cadastre-se</a></li>
-                    <li id="navlink"><a href="modal_login">Entrar</a></li>
+                    <li id="navlink-sigin"><a href="modal_sigin">Cadastre-se</a></li>
+                    <li id="navlink-login"><a className="log" href="modal_login">Entrar</a></li>
                 </ul>
             </nav>
         );
