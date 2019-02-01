89bcede812bdebc7c02b218dcf0ccdc16da0cb29
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-02-01 17:18:21 -0300

    planos

--- src/components/Kinvoweb/index.js
+++ src/components/Kinvoweb/index.js
@@ -7,12 +7,14 @@ class Kinvoweb extends Component {
         return (
             { /* kinvoweb */},
             <section id= "kinvoweb">
-            <h2>kinvoWeb</h2>
-            <h3><span>Kinvo.</span>Na palma da mão e na tela do computador</h3>
-            <div>
-                <img src= { kinvowebbg } alt="kinvoweb"/>
+            <div id="col" className="col-1">
+                <h2>kinvoWeb</h2>
+                <h3><span>Kinvo. </span>Na palma da mão e na tela do computador</h3>
+                <p>Além do aplicativo para smartphone, o Kinvo também conta com uma versão para computador. Você terá acesso a todos os Recursos Avançados disponíveis na versão mobile, com todo o conforto de uma visualização ampla, fácil de usar e com total segurança dos seus dados. Os recursos da versão para computador são exclusivos para assinantes e serão ativados junto com a escolha de um plano. Para acessar a ferramenta, é necessário antes baixar o aplicativo no seu celular.</p>
             </div>
-            <p>Além do aplicativo para smartphone, o Kinvo também conta com uma versão para computador. Você terá acesso a todos os Recursos Avançados disponíveis na versão mobile, com todo o conforto de uma visualização ampla, fácil de usar e com total segurança dos seus dados. Os recursos da versão para computador são exclusivos para assinantes e serão ativados junto com a escolha de um plano. Para acessar a ferramenta, é necessário antes baixar o aplicativo no seu celular.</p>
+            <div id="col" className="col-img-right">
+                <img id="img-kinvoweb"src= { kinvowebbg } alt="kinvoweb"/>
+            </div>
             </section>
         );
     }
--- src/components/Planos/index.js
+++ src/components/Planos/index.js
@@ -5,44 +5,35 @@ class Planos extends Component {
     render() {
         return (
             <section id="planos">
+            <div id="col" className="text">
             <h2>Planos e assinaturas</h2>
             <h3><span>Conheça os planos e</span>Use todos os recursos</h3>
+            <p>O Kinvo já é completo desde a versão gratuita. Com ele você pode cadastrar diversos tipos de produtos. Assinando o Kinvo, você terá acesso aos Recursos Avançados de Análises e às Múltiplas Carteiras, que são recursos complementares e indispensáveis que te ajudam a ter uma visão completa do seu dinheiro, auxiliam nas tomadas de decisão e organizam seus investimentos. Conheça os nossos planos. Você poderá cancelar a assinatura a qualquer momento.</p>
+            </div>
             { /* card deck 2 */ }
-            <div id="card_deck">
+            <div id="col" className="card-deck">
                 { /* linha de card 1x1 */ }
-                <div id="card_ln">
+                <div id="">
                     { /* card */ }
-                    <div id="card">
+                    <div id="card-set">
                         { /* card-plain */ }
-                        <h5></h5>
+                        <div id="card-title">
+                            <h5>Plano Básico</h5>
+                        </div>
+                        <div id="card-content">
                         { /* card-content */ }
-                        <p></p>
-                        { /* card-mark */ }
-                        <h6></h6>
-                    </div>
-                </div>
-                { /* linha de card 2x1 */ }
-                <div id="card_ln">
-                    { /* card */ }
-                    <div id="card">
-                        { /* card-plain */ }
-                        <h5></h5>
-                        { /* card-content */ }
-                        <p></p>
-                        { /* card-mark */ }
-                        <h6></h6>
-                    </div>
-                </div>
-                { /* linha de card 3x1 */ }
-                <div id="card_ln">
-                    { /* card */ }
-                    <div id="card">
-                        { /* card-plain */ }
-                        <h5></h5>
-                        { /* card-content */ }
-                        <p></p>
-                        { /* card-mark */ }
-                        <h6></h6>
+                            <ul>
+                                <li>1 carteira</li>
+                                <li>15 produtos</li>
+                                <li>Produtos iniciados a parti de 2012</li>
+                                <li>Análisea básica</li>
+                                <li>Gráfico de desempenho</li>
+                            </ul>
+                        </div>
+                        <div id="card-mark">
+                            { /* card-mark */ }
+                            <h5>Gratuito</h5>
+                        </div>
                     </div>
                 </div>
             </div>
