ba49ed7904b72ee5fabfc9f717ea5ea544f7c562
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-02-03 12:27:52 -0300

    navbar efeccts

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,9 +4,32 @@ import React, {Component} from 'react';
 import "./style.css";
 import logo from "./logo.png";
 class Navbar extends Component {
+    // construtor do componenmt
+    state = {
+        bgcolor_nav: 'transparent',
+        bgcolor_login: '#0DD1E3',
+        bgcolor_sigin: '#ffffff',
+        bordercolor_login: 'transparent',
+        bordercolor_sigin: 'transparent',
+        
+    }
+      
+      listenScrollEvent = e => {
+        if (window.scrollY > 600) {
+            this.setState({bgcolor_nav: '#0DD1E3'})
+            this.setState({bordercolor_sigin: '#ffffff'})
+        } else {
+            this.setState({bgcolor_nav: 'transparent'})
+            this.setState({bordercolor_sigin: 'transparent'})
+        }
+      }
+      componentDidMount() {
+        window.addEventListener('scroll', this.listenScrollEvent)
+      }
+        
     render() {
         return (
-            <nav className="menu">
+            <nav className="menu" style={{backgroundColor: this.state.bgcolor_nav}}>
                 <a className="Navbar-brand">
                     <img id="nav-brand" src= {logo} alt="logo kinvo"/>
                 </a>
@@ -16,12 +39,13 @@ class Navbar extends Component {
                     <li id="navlink"><a href="#kinvoweb">Kinvo Web</a></li>
                     <li id="navlink"><a href="#planos">Kinvo Web</a></li>
                     <li id="navlink"><a href="#quem_faz">Quem faz</a></li>
-                    <li id="navlink-sigin"><a href="modal_sigin">Cadastre-se</a></li>
+                    <li id="navlink-sigin"><a href="modal_sigin" style={{borderColor: this.state.bordercolor_sigin}}>Cadastre-se</a></li>
                     <li id="navlink-login"><a className="log" href="modal_login">Entrar</a></li>
                 </ul>
             </nav>
         );
     }
 }
+
 
 export default Navbar;
